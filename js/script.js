e7a5fda6c5d5f8b2b58347568db001c1d57d5626
Viji-Alagiri1 <va663@nyu.edu>
2016-04-09 14:13:23 -0400

    1st commit
    Final Project

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var map = new L.Map('map', {
+    scrollWheelZoom: false
+  }).setView( [40.705107,-73.990860], 10);
+
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
+     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    }).addTo(map);
+
+  var layerURL = 'https://va663.cartodb.com/api/v2/viz/adfb27da-fdba-11e5-8229-0e3ff518bd15/viz.json';
+
+//https://va663.cartodb.com/api/v2/viz/adfb27da-fdba-11e5-8229-0e3ff518bd15/viz.json
+   
+   //code from dmiketa/final (http://github.com/dmiketa/final)
+  cartodb.createLayer(map, layerURL)
+      .addTo(map)
+      .on('done', function(layer) {
+     sublayer = layer.getSubLayer(0);
+     sublayer.show();
+     sublayer2 = layer.getSubLayer(1);
+     sublayer2.hide();
+     
+  })
+    .on('error', function() {
+    //log the error
+  });
+  $(".button").click(function() {
+      if($(this).attr('id') == 'borough' ) {
+        sublayer.show();
+        sublayer2.hide();
+        
+      }
+      if($(this).attr('id') == 'rest' ) {
+         sublayer2.show();
+        sublayer.hide();
+      }
+   });
